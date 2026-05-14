f52d72d329394f8416d2f7a02c117679004f93b5
sonah <samuelonah81@gmail.com>
2026-05-14 21:51:11 +0100

    updat

---
+++ ascii-art project/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var FontStyle string
+
+	if len(os.Args) == 2 {
+		FontStyle = "standard.txt"
+	} else if len(os.Args) == 3 {
+		FontStyle = os.Args[2]
+	} else {
+		fmt.Println("Usage: go run . [Your text] [Fontstyle]")
+		return
+	}
+	bannerMap, err := LoadBanner(FontStyle)
+	if err != nil {
+		fmt.Println("Oops. File not found")
+		return
+	}
+	lines := splitLines(os.Args[1])
+	for _, line := range lines {
+		if line == "" {
+			fmt.Println()
+			continue
+		}
+		_, err := validate(line)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Print(generateAscii(line, bannerMap))
+	}
+}
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	var FontStyle string
+
+	if len(os.Args) == 2 {
+		FontStyle = "standard.txt"
+	} else if len(os.Args) == 3 {
+		FontStyle = os.Args[2]
+	} else {
+		fmt.Println("Usage: go run . [Your text] [Fontstyle]")
+		return
+	}
+	bannerMap, err := LoadBanner(FontStyle)
+	if err != nil {
+		fmt.Println("Oops. File not found")
+		return
+	}
+	lines := splitLines(os.Args[1])
+	for _, line := range lines {
+		if line == "" {
+			fmt.Println()
+			continue
+		}
+		_, err := validate(line)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Print(generateAscii(line, bannerMap))
+	}
+}

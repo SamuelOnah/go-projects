eadcfb10e6ac0fd3303ba5ab783033578865dc4a
sonah <samuelonah81@gmail.com>
2026-06-03 14:40:07 +0100

    modified

--- ascii-PhaseTwo/generate.go
+++ ascii-PhaseTwo/generate.go
@@ -2,7 +2,7 @@ package generate
 
 func GeneratePattern(c rune) []string {
 	style := map[rune][]string{
-		
+
 		'Z': {
 			" #### ",
 			"    # ",
@@ -14,10 +14,9 @@ func GeneratePattern(c rune) []string {
 			"      ",
 		},
 	}
+	for i := 'A'; i < 'Z'; i++ {
+		style[i] = []string{
 
-	for x :='A'; x < 'Z'; x++{
-		style[x] = []string{
-			
 			"  ##  ",
 			" #  # ",
 			" #  # ",
@@ -26,20 +25,14 @@ func GeneratePattern(c rune) []string {
 			" #  # ",
 			" #  # ",
 			"      ",
-	
 		}
 	}
 	var res []string
 	data, ok := style[c]
-
-	if !ok{
+	if !ok {
 		return []string{}
 	}
-	art := make([]string, 8)
-	for range 8 {
-		art = data[:]
-	}
+	art := data[:]
 	res = append(res, art...)
-
 	return res
 }
